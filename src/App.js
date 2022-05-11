e873372b87e7c4d21920168abb72158b75d4aef7
DEENNO <yuntteng31@gmail.com>
2022-05-11 17:32:54 +0900

    코인 환전기능 구현

--- src/App.js
+++ src/App.js
@@ -2,9 +2,27 @@ import { useState, useEffect } from "react";
 
 function App() {
   const [loading, setLoading] = useState(true);
+  const [calculate, setCalulate] = useState(false);
   const [coins, setCoins] = useState([]);
+  const [selectCoin, setSelectCoin] = useState(0);
+  const [money, setMoney] = useState(0);
+  const [result, setResult] = useState(0);
+  const onClick = () => {
+    setCalulate((prev) => !prev);
+  };
+  const showValue = (e) => setSelectCoin(() => e.target.value);
+  const onChange = (e) => setMoney(() => e.target.value);
+
   useEffect(() => {
-    fetch("https://api.coinpaprika.com/v1/tickers?limit=4000")
+    if (money > 0 && selectCoin > 0) {
+      setResult(money / selectCoin);
+    }
+    // console.log("현금 : " + money);
+    // console.log("코인 : " + selectCoin); //우리가 선택한 코인 금액
+  }, [calculate]);
+
+  useEffect(() => {
+    fetch("https://api.coinpaprika.com/v1/tickers?limit=400")
       .then((response) => response.json())
       .then((json) => {
         setCoins(json);
@@ -16,13 +34,21 @@ function App() {
       <h1>The Coins! : {loading ? "" : coins.length}</h1>
       {loading ? <strong>Loading...</strong> : null}
       <hr />
-      <ul>
+      <select name="money" size="5" onChange={showValue}>
+        <option>원하는 코인을 선택하세요</option>
         {coins.map((coin) => (
-          <li key={coin.id}>
+          <option key={coin.id} value={coin.quotes.USD.price}>
             {coin.name} ({coin.symbol}) : ${coin.quotes.USD.price} USD
-          </li>
+          </option>
         ))}
-      </ul>
+      </select>
+      <div>
+        <h2>현금 얼마 투자할래?(한국 돈 기준)</h2>
+        <input type="number" onChange={onChange}></input>
+        <input type="button" value="Exchange" onClick={onClick} />
+        <h3>{result}</h3>
+        <p>의 코인 환전이 가능합니다.</p>
+      </div>
     </div>
   );
 }
