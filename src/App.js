c3651a6774134eb50ba049db9b4f1304c5100e6c
DEENNO <yuntteng31@gmail.com>
2022-05-13 18:19:52 +0900

    영화 정보 API

--- src/App.js
+++ src/App.js
@@ -2,53 +2,40 @@ import { useState, useEffect } from "react";
 
 function App() {
   const [loading, setLoading] = useState(true);
-  const [calculate, setCalulate] = useState(false);
-  const [coins, setCoins] = useState([]);
-  const [selectCoin, setSelectCoin] = useState(0);
-  const [money, setMoney] = useState(0);
-  const [result, setResult] = useState(0);
-  const onClick = () => {
-    setCalulate((prev) => !prev);
+  const [movies, setMovies] = useState([]);
+  const getmovies = async () => {
+    const json = await (
+      await fetch(
+        `https://yts.mx/api/v2/list_movies.json?minimum_rating=8.8&sort_by=year`
+      )
+    ).json();
+    setMovies(json.data.movies);
+    setLoading(false);
   };
-  const showValue = (e) => setSelectCoin(() => e.target.value);
-  const onChange = (e) => setMoney(() => e.target.value);
-
   useEffect(() => {
-    if (money > 0 && selectCoin > 0) {
-      setResult(money / selectCoin);
-    }
-    // console.log("현금 : " + money);
-    // console.log("코인 : " + selectCoin); //우리가 선택한 코인 금액
-  }, [calculate]);
-
-  useEffect(() => {
-    fetch("https://api.coinpaprika.com/v1/tickers?limit=400")
-      .then((response) => response.json())
-      .then((json) => {
-        setCoins(json);
-        setLoading(false);
-      });
+    getmovies();
   }, []);
+  console.log("영화입니다" + JSON.stringify(movies, null, 4));
   return (
     <div>
-      <h1>The Coins! : {loading ? "" : coins.length}</h1>
-      {loading ? <strong>Loading...</strong> : null}
-      <hr />
-      <select name="money" size="5" onChange={showValue}>
-        <option>원하는 코인을 선택하세요</option>
-        {coins.map((coin) => (
-          <option key={coin.id} value={coin.quotes.USD.price}>
-            {coin.name} ({coin.symbol}) : ${coin.quotes.USD.price} USD
-          </option>
-        ))}
-      </select>
-      <div>
-        <h2>현금 얼마 투자할래?(한국 돈 기준)</h2>
-        <input type="number" onChange={onChange}></input>
-        <input type="button" value="Exchange" onClick={onClick} />
-        <h3>{result}</h3>
-        <p>의 코인 환전이 가능합니다.</p>
-      </div>
+      {loading ? (
+        <h1>Loading</h1>
+      ) : (
+        <div>
+          {movies.map((movie) => (
+            <div key={movie.id}>
+              <img src={movie.medium_cover_image} />
+              <h2>{movie.title}</h2>
+              <p>{movie.summary}</p>
+              <ul>
+                {movie.genres.map((g) => (
+                  <li key={g}>{g}</li>
+                ))}
+              </ul>
+            </div>
+          ))}
+        </div>
+      )}
     </div>
   );
 }
